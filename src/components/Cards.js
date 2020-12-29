a9615249674e51536ac35fe6b33ff5223d9b4871
Kenneth Wong <kenweb2011@gmail.com>
2020-12-29 10:15:37 -0600

    Update image path

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,7 +10,7 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem 
-              src="images/img-9.jpg"
+              src="./images/img-9.jpg"
               text="Explore the hidden waterfall deep
               inside the Amazon Jungle"
               label="Adventure"
@@ -26,7 +26,7 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='/images/img-3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
